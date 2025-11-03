5a164c17bbb3a970e0e41e85c016364e2aa93421
Asma147 <trabelsi.asma@esprit.tn>
2025-11-03 21:19:52 +0100

    Ajout du prosit 7

---
+++ prosit7/src/entities/Animal.java
@@ -0,0 +1,40 @@
+package entities;
+
+public class Animal {
+  public   String family;
+   public String name;
+  public int age;
+   public boolean isMammal;
+
+
+    public Animal() {
+    }
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+    public Animal(String family, String name, int age, boolean isMammal) {
+        this.family = family;
+        this.name = name;
+        this.age = age;
+        this.isMammal = isMammal;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Family: " + family + ", Name: " + name + ", Age: " + age + ", Mammal: " + isMammal;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ prosit7/src/entities/Penguin.java
@@ -0,0 +1,33 @@
+package entities;
+
+public class Penguin extends Aquatic {
+    protected float swimmingDepth;
+
+
+    public Penguin() {
+    }
+
+
+    public Penguin(String family, String name, int age, boolean isMammal, String habitat, float swimmingDepth) {
+        super(family, name, age, isMammal, habitat);
+        this.swimmingDepth = swimmingDepth;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Swimming Depth: " + swimmingDepth + " meters";
+    }
+
+    public float getSwimmingDepth() {
+        return swimmingDepth;
+    }
+
+    public void setSwimmingDepth(float swimmingDepth) {
+        this.swimmingDepth = swimmingDepth;
+    }
+
+    @Override
+    public void swim() {
+
+    }
+}
