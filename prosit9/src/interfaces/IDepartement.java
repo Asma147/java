04cf42d908eef99079baaf24a7cfc0afebe3a185
Asma147 <trabelsi.asma@esprit.tn>
2025-11-18 21:17:26 +0100

    Ajout du prosit 9

---
+++ prosit9/src/interfaces/IDepartement.java
@@ -0,0 +1,11 @@
+package interfaces;
+
+public interface IDepartement<T> {
+    void ajouterDepartement(T t);
+    boolean rechercherDepartement(String nomDepartement);
+    boolean rechercherDepartement(T t);
+    void supprimerDepartement(T t);
+    void afficherDepartements();
+    void trierDepartementsParId();
+    void trierDepartementsParNomEtNombreEmployes();
+}
